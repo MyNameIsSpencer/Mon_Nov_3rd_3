fefa77d9dbe8c2990923f0adef89a9af3a620a50
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-08 20:16:29 -0500

    functioning products in cart

--- products.rb
+++ products.rb
@@ -1,4 +1,4 @@
-require_relative 'shopping_cart.rb'
+
 
 puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
 
@@ -10,15 +10,32 @@ class Products
     @name = name
     @base_price = base_price
     @tax_rate = @@tax_money
-    @total_price = 0
+
   end
 
-  def create(name, base_price)
-    new_product = Products(name, base_price)
-    return new_product
+  
+  # def create(name, base_price)
+  #   new_product = Products(name, base_price)
+  #   return new_product
+  # end
+
+  ## READERS
+
+  def name_reader
+    @name
   end
 
-  def calculate_tot
+  def base_reader
+    @base_price
+  end
+
+  def tax_reader
+    @tax_rate
+  end
+
+
+
+  def price_total
     @total_price = @base_price + (@base_price * @tax_rate)
   end
 
--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,15 +1,39 @@
+require_relative 'products.rb'
+
 puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
 
 class Cart
 
+  @@shopping_cart = []
+
+  def self.create(name, base_price)
+    @@shopping_cart << Products.new(name, base_price)
+  end
+
+  def remove(product)
+    @@shopping_cart.each do |item|
+      break if item.name == product
+        @@shopping_cart.delete(item)
+
+    end
+  end
+
+  def self.in_cart
+    @@shopping_cart
+  end
 
 
+end
 
 
+super_computer = Cart.create('Super Computer', 200000)
+death_ray1 = Cart.create('Death Ray', 45000)
+death_ray2 = Cart.create('Death Ray', 45000)
+death_ray3 = Cart.create('Death Ray', 45000)
+mega_jet = Cart.create('Mega Jet', 13000000)
+robot_henchman = Cart.create('Robot Henchman', 150000)
 
-
-
-
-
+puts Cart.in_cart.count
+puts Products.price_total
 
 puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
